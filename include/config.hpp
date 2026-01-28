1673978293fae45a6c1f6f5899cda70a262731c0
TfOi9 <tfoi92024@sjtu.edu.cn>
2026-01-28 22:43:43 +0800

    feat: comparator for any fixed type

--- include/config.hpp
+++ include/config.hpp
@@ -13,6 +13,13 @@ static_assert(PAGE_SLOT_COUNT % 2 == 0, "Slot count must be even!");
 
 constexpr size_t CACHE_CAPACITY = 500;
 
+typedef int64_t hash_t;
+
+constexpr hash_t HASH_MOD1 = 998244353;
+constexpr hash_t HASH_MOD2 = 1000000007;
+constexpr hash_t HASH_BASE1 = 10007;
+constexpr hash_t HASH_BASE2 = 9973;
+
 } // namespace sjtu
 
 #endif // CONFIG_HPP
