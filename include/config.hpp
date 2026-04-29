caf650244c2a82c948daa5e206884dd210feeeb5
TfOi9 <tfoi92024@sjtu.edu.cn>
2026-04-29 22:55:48 +0800

    test: enlarge page size

--- include/config.hpp
+++ include/config.hpp
@@ -8,10 +8,10 @@ namespace sjtu {
 
 typedef int64_t diskpos_t;
 
-constexpr size_t PAGE_SLOT_COUNT = 100;
+constexpr size_t PAGE_SLOT_COUNT = 110;
 static_assert(PAGE_SLOT_COUNT % 2 == 0, "Slot count must be even!");
 
-constexpr size_t CACHE_CAPACITY = 3400;
+constexpr size_t CACHE_CAPACITY = 2900;
 
 typedef int64_t hash_t;
 
